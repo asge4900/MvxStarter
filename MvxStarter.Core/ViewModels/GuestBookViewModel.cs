a976771cd4da1ee518731e5cb978830a7a6845a2
asge4900 <48907330+asge4900@users.noreply.github.com>
2021-03-03 12:37:08 +0100

    First commit

---
+++ MvxStarter.Core/ViewModels/GuestBookViewModel.cs
@@ -0,0 +1,74 @@
+﻿using MvvmCross.Commands;
+using MvvmCross.ViewModels;
+using MvxStarter.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace MvxStarter.Core.ViewModels
+{
+    public class GuestBookViewModel : MvxViewModel
+    {
+        private ObservableCollection<PersonModel> people = new ObservableCollection<PersonModel>();
+       
+        private string firstName;
+
+        private string lastName;
+
+        public GuestBookViewModel()
+        {
+            AddGuestCommand = new MvxCommand(AddGuest);                
+        }
+
+
+        public ObservableCollection<PersonModel> People
+        {
+            get => people;
+            set => SetProperty(ref people, value);
+        }
+
+        public string FirstName
+        {
+            get => firstName;
+            set
+            {
+                SetProperty(ref firstName, value);
+                RaisePropertyChanged(() => FullName);
+                RaisePropertyChanged(() => CanAddGuest);
+            }
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                SetProperty(ref lastName, value);
+                RaisePropertyChanged(() => FullName);
+                RaisePropertyChanged(() => CanAddGuest);
+            }
+        }
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        public bool CanAddGuest => FirstName?.Length > 0 && LastName?.Length > 0;
+
+        public IMvxCommand AddGuestCommand { get; set; }
+
+
+        public void AddGuest()
+        {
+            PersonModel p = new PersonModel
+            {
+                FirstName = FirstName,
+                LastName = LastName
+            };
+
+            FirstName = string.Empty;
+            LastName = string.Empty;
+
+            People.Add(p);
+        }
+    }
+}
